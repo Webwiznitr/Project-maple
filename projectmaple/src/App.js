dcb9b29531c6d752f09d2962dbd7da1a2e709188
Raghavbajaj <Raghavbajaj@users.bajaj.github.com>
2021-02-27 13:43:18 +0530

    Applying the authentication for the user and saving the data in the local storage

--- projectmaple/src/App.js
+++ projectmaple/src/App.js
@@ -1,8 +1,16 @@
+import React from "react";
 import "./App.css";
 import Navbaroverlay from "./Navbar-overlay";
 import Navbar from "./Navbar";
+import Auth from "./Auth";
+import useToken from "./useToken";
 
 function App() {
+  const { token, setToken } = useToken();
+
+  if (!token) {
+    return <Auth setToken={setToken} />;
+  }
   return (
     <div className="App">
       <Navbaroverlay />
--- projectmaple/src/Auth.js
+++ projectmaple/src/Auth.js
@@ -1,15 +1,51 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Auth.css";
-function Auth() {
+import PropTypes from "prop-types";
+
+async function loginUser(credentials) {
+  return fetch("http://localhost:8080/Auth", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(credentials),
+  })
+    .then((data) => data.json())
+    .catch((err) => console.error(err));
+}
+
+function Auth({ setToken }) {
+  const [username, setUserName] = useState();
+  const [password, setPassword] = useState();
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    const token = await loginUser({
+      username,
+      password,
+    });
+    console.log(token);
+    setToken(token);
+  };
   return (
     <div className="login-wrapper">
       <div className="login-img">
         <img src="https://c7.uihere.com/files/497/432/35/5bbafc5febe2e.jpg" />
       </div>
-      <form>
+      <form onSubmit={handleSubmit}>
         <h1 className="login">Please Log In</h1>
-        <input type="text" placeholder="    Enter your Username" />
-        <input type="password" placeholder="    Enter your password" />
+        <input
+          type="text"
+          placeholder="    Enter your Username"
+          value={username}
+          onChange={(e) => setUserName(e.target.value)}
+        />
+        <input
+          type="password"
+          value={password}
+          placeholder="    Enter your password"
+          onChange={(e) => setPassword(e.target.value)}
+        />
 
         <div>
           <button type="submit">Submit</button>
@@ -18,5 +54,9 @@ function Auth() {
     </div>
   );
 }
+
+Auth.propTypes = {
+  handlenew: PropTypes.func.isRequired,
+};
 
 export default Auth;
