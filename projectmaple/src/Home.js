e18b7f81f3391b381294ea5f9d2d472dfcd805fc
Raghavbajaj <Raghavbajaj@users.bajaj.github.com>
2021-03-04 15:34:39 +0530

    Change  background

--- projectmaple/src/Home.js
+++ projectmaple/src/Home.js
@@ -1,9 +1,36 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+
+import "./App.css";
 
 function Home() {
+  const [imgtransition, setimgtransition] = useState("");
+
+  const listenScrollEvent = () => {
+    window.scrollY > 10
+      ? setimgtransition("foodimg-fadeout")
+      : setimgtransition("");
+  };
+
+  useEffect(() => {
+    window.addEventListener("scroll", listenScrollEvent);
+  });
+
   return (
-    <div>
-      <div id="first-solid-bar"></div>
+    <div id="first-solid-bar">
+      <div className="cafefood">
+        <div className={`cafefood-img  ${imgtransition}`}></div>
+        <div className="cafefood-text">
+          <h3>Your Order Sir </h3>
+          <p>
+            Brand style guides have historically been presented as a large
+            document, complete with headers. Uber's approach to digitally
+            presenting their brand guidelines excels at presenting a large
+            volume of information in an easy-to-use, intuitive format. Users can
+            quickly and easily navigate through this simple, forward-thinking
+            menu.
+          </p>
+        </div>
+      </div>
     </div>
   );
 }
